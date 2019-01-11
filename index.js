2e84a62d7f96b71e4495b5178ef869ecb4b4ac48
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-11 07:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  const url = `https://api.github.com/repos/${repo}/forks`
-  //use fetch to fork it!
-  fetch(repo)
+  const url = `https://api.github.com/repos/${repo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
   .then(result => result.json())
   .then(json => showResults(json));
 }
