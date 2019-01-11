3252d78aa25bebfcf7cd3cfeb3e7f452f192b0e7
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-11 07:26:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const base = 'https://api.github.com/repos';
+const baseURL = 'https://api.github.com/repos';
 const user = 'FKRauber';
 
 function getToken() {
@@ -9,28 +9,31 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  const url = `${base}/${repo}/forks`;
+  const url = `${baseURL}/repos/${repo}/forks`;
   fetch(url, {
     method: 'POST',
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
-  .then(result => result.json())
-  .then(json => showResults(json));
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
 
 function showResults(json) {
-  document.getElementById('results').innerHTML = `<a href="${json.html_url}">${json.htmml_url}</a>`;
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
+    json.html_url
+  }</a>`;
 }
 
 function createIssue() {
   const repo = `${user}/js-ajax-fetch-lab`;
-  const url = `${base}/${repo}/issues`;
+  const url = `${baseURL}/repos/${repo}/issues`;
   const postData = {
     title: document.getElementById('title').value,
-    body: document.getElementById('body').value;
-  }
+    body: document.getElementById('body').value
+  };
+
   fetch(url, {
     method: 'POST',
     body: JSON.stringify(postData),
@@ -38,18 +41,18 @@ function createIssue() {
       Authorization: `token ${getToken()}`
     }
   })
-  .then(result => result.json())
-  .then(json => getIssues());
+    .then(res => res.json())
+    .then(json => getIssues());
 }
 
 function getIssues() {
   const repo = `${user}/js-ajax-fetch-lab`;
-  const url = `${base}/${repo}/issues`;
+  const url = `${baseURL}/repos/${repo}/issues`;
   fetch(url, {
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
-  .then(result => result.json())
-  .then(json => console.log(json));
+    .then(res => res.json())
+    .then(json => console.log(json));
 }
