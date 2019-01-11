566b247d2a472ea0e20d60c29560d94794f17ef2
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-11 07:14:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,17 +19,12 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
-  document.getElementById('results').innerHTML = `<a href="${json.html_url}">`
-
-  return `
-    <div>
-      <p><a href="${json.html_url}" ></a></p>
-    </div>
-  `
+  document.getElementById('results').innerHTML = `<a href="${json.html_url}">${json.htmml_url}</a>`;
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `https://api.github.com/repos/${repo}/issues`;
 }
 
 function getIssues() {
